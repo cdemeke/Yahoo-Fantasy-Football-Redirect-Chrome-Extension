66b0b469b544eedca0f768f91bec9a7eb66fdd3a
Christian Demeke <demeke2@gmail.com>
2017-08-29 10:12:40 -0400

    Init

---
+++ background.js
@@ -0,0 +1,14 @@
+var host = "https://football.fantasysports.yahoo.com//";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://yahoo.com/*",
+            "*://www.yahoo.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
